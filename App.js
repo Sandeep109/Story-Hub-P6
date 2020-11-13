c7170adfebf71dafe370a44f84980d384945f41d
sandeep109 <sandeephshakuniya@gmail.com>
2020-11-13 13:50:53 +0530

    adding files

--- App.js
+++ App.js
@@ -1,44 +1,67 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
-import { createAppContainer } from 'react-navigation';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer ,createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 import ReadStoryScreen from './screens/ReadStoryScreen';
 import WriteStoryScreen from './screens/WriteStoryScreen';
+import LoginScreen from './screens/LoginScreen'
 
 export default class App extends React.Component {
-  render() {
-    return(
-       <AppContainer />
-    ) 
+  render(){
+    return (
+
+        <AppContainer/>
+
+    );
   }
 }
 
+
+
 const TabNavigator = createBottomTabNavigator({
-    WriteStory: { screen: WriteStoryScreen },
-    ReadStory: { screen: ReadStoryScreen },
-  },
-  {
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({}) => {
-        const routeName = navigation.state.routeName;
-        if (routeName === 'ReadStory') {
-          return (
-            <Image
-              source={require('./assets/read.png')}
-              style={{ width: 30, height: 30 }}
-            />
-          );
-        } else if (routeName === 'WriteStory') {
-          return (
-            <Image
-              source={require('./assets/write.png')}
-              style={{ width: 30, height: 30 }}
-            />
-          );
-        }
-      },
-    }),
-  }
+  write: {screen: WriteStoryScreen},
+  read: {screen: ReadStoryScreen},
+},
+{
+  defaultNavigationOptions: ({navigation})=>({
+    tabBarIcon: ()=>{
+      const routeName = navigation.state.routeName;
+      console.log(routeName)
+      if(routeName === "write"){
+        return(
+          <Image
+          source={require("./assets/write.png")}
+          style={{width:40, height:40}}
+        />
+        )
+
+      }
+      else if(routeName === "read"){
+        return(
+          <Image
+          source={require("./assets/read.png")}
+          style={{width:40, height:40}}
+        />)
+
+      }
+    }
+  })
+}
 );
 
-const AppContainer = createAppContainer(TabNavigator);
+const switchNavigator = createSwitchNavigator({
+LoginScreen:{screen: LoginScreen},
+TabNavigator:{screen: TabNavigator}
+})
+
+const AppContainer =  createAppContainer(switchNavigator);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
